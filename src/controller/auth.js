912d33dd69530c9359586b52e9b59d736f698851
spliatz <spliatzoff@gmail.com>
2022-11-25 15:56:18 +0400

    fix(controller): поправил отправление ошибки на клиент

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -32,7 +32,7 @@ class AuthController {
             return res.status(200).json({id})
         } catch (e) {
             console.log(e)
-            res.status(500).json(e)
+            return res.status(500).json({message: e.message})
         }
     }
 
